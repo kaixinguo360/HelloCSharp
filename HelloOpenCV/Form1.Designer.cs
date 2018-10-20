cc4056165d29bc86573a7ff69a2ecf1f74f58da5
Kaixinguo <kaixinguo@kaixinguo.site>
2018-10-20 18:02:56 +0800

    Face Detection Dome

--- HelloOpenCV/Form1.Designer.cs
+++ HelloOpenCV/Form1.Designer.cs
@@ -32,16 +32,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 435);
+            this.button1.Location = new System.Drawing.Point(257, 435);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(151, 37);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Open";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -62,11 +63,22 @@
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 484);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.imageBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -83,6 +95,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- HelloOpenCV/Form1.cs
+++ HelloOpenCV/Form1.cs
@@ -11,19 +11,76 @@ using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.Util;
 using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
 
 namespace HelloOpenCV
 {
     public partial class Form1 : Form
     {
+        private static VideoCapture cameraCapture;
+        CascadeClassifier cascadeClassifier;
+
+        Image<Bgr, byte> img;
+
         public Form1()
         {
             InitializeComponent();
+
+            cascadeClassifier = new CascadeClassifier(@"F:\Tools\OpenCV\build\etc\haarcascades\haarcascade_frontalface_alt.xml");
+
+            Application.Idle += new EventHandler(delegate
+            {
+                if(img != null)
+                {
+                    imageBox1.Image = ProcessImage(img);
+                }
+            });
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "Hello,C#!";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "BMP文件|*.bmp|JPG文件|*.jpg|JPEG文件|*.jpeg|所有文件|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(openFileDialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenCamera();
+            Application.Idle += new EventHandler(delegate
+            {
+                img = cameraCapture.QueryFrame().ToImage<Bgr, byte>();
+            });
+        }
+
+        private void OpenCamera()
+        {
+            try
+            {
+                cameraCapture = new VideoCapture();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
+        }
+
+        private Image<Bgr, byte> ProcessImage(Image<Bgr, byte> img)
+        {
+            var grayframe = img.Convert<Gray, byte>();
+            var faces = cascadeClassifier.DetectMultiScale(grayframe);
+
+            foreach (var face in faces)
+            {
+                img.Draw(face, new Bgr(0, double.MaxValue, 0), 3);
+            }
+            return img;
         }
     }
 }
