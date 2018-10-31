3f21e034182258900e3429c64b3cd118cff4187c
Kaixinguo360 <kaixinguo360@gmail.com>
2018-10-31 15:11:54 +0800

    Optimize...

--- HelloOpenCV/Form1.Designer.cs
+++ HelloOpenCV/Form1.Designer.cs
@@ -32,59 +32,135 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(491, 678);
+            this.button1.Location = new System.Drawing.Point(887, 618);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 37);
+            this.button1.Size = new System.Drawing.Size(220, 37);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open";
+            this.button1.Text = "运行";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(745, 39);
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(887, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(166, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "No Key Point";
             // 
             // imageBox1
             // 
             this.imageBox1.Location = new System.Drawing.Point(12, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(953, 645);
+            this.imageBox1.Size = new System.Drawing.Size(869, 643);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
-            // button2
+            // listBox1
             // 
-            this.button2.Location = new System.Drawing.Point(334, 678);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 37);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Run";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(887, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 259);
+            this.listBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(888, 353);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 259);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "显示项目";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 75);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 19);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "关键点";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 50);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "监测点";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "检测框";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(884, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "关键点";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(884, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "检测点";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 727);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1119, 673);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.imageBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,7 +171,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private Emgu.CV.UI.ImageBox imageBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- HelloOpenCV/Form1.cs
+++ HelloOpenCV/Form1.cs
@@ -20,61 +20,75 @@ namespace HelloOpenCV
     public partial class Form1 : Form
     {
         private static VideoCapture cameraCapture;
-        CascadeClassifier cascadeClassifier;
-        Net net;
 
-        Image<Bgr, byte> img;
+        private Net net;
+
+        private Image<Bgr, byte> img;
+
+        private bool showBoxs = false;
+        private bool showPoints = false;
+        private bool showKeyPoint = false;
+
+
+        ////////////////
+        /// 窗口操作 ///
+        ////////////////
 
         public Form1()
         {
             InitializeComponent();
 
-            cascadeClassifier = new CascadeClassifier(@"Properties\haarcascade_frontalface_alt.xml");
+            net = DnnInvoke.ReadNetFromTensorflow(@"Properties\frozen_inference_graph.pb", @"Properties\graph.pbtxt");  //载入神经网络
 
-            net = DnnInvoke.ReadNetFromTensorflow(@"Properties\frozen_inference_graph.pb", @"Properties\graph.pbtxt");
-
-            if (net == null)
+            if (net == null) //如果载入失败
             {
-                throw new Exception("Error");
+                throw new Exception("Error"); //报错
             }
 
-            Application.Idle += new EventHandler(delegate
+            Application.Idle += new EventHandler(delegate  //于空闲状态时执行以下代码
             {
-                if (img != null)
-                {
-                    //img = ProcessImage(img);
-                    img = ProcessImageUseDnn(img);
-                    img = DrawPoints(img);
-                    img = DrawKeyPoint(img);
-                    imageBox1.Image = img;
+                if (img != null)                                //如果摄像头有传入图像
+                {                                               //则依次经过以下图像处理方法
+                    img = ProcessImageUseDnn(img);              //  1.DNN网络手部检测方法
+                    if (showPoints) img = DrawPoints(img);      //  2.检测点绘制方法
+                    if (showKeyPoint) img = DrawKeyPoint(img);  //  3.关键点绘制方法
+                    imageBox1.Image = img;                      //最后输出图像到 imageBox1
 
                 }
             });
-
-            button2_Click(null, null);
         }
-
+        
+        /***** 运行按钮 点击事件 ****/
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Hello,C#!";
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "BMP文件|*.bmp|JPG文件|*.jpg|JPEG文件|*.jpeg|所有文件|*.*";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                img = new Image<Bgr, byte>(openFileDialog.FileName);
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
             OpenCamera();
-            Application.Idle += new EventHandler(delegate
+            Application.Idle += new EventHandler(delegate    //于空闲状态时执行以下代码
             {
-                img = cameraCapture.QueryFrame().ToImage<Bgr, byte>();
-                CvInvoke.Flip(img.Mat, img.Mat, FlipType.Horizontal);
+                img = cameraCapture.QueryFrame().ToImage<Bgr, byte>(); //从摄像头获取一帧图像
+                CvInvoke.Flip(img.Mat, img.Mat, FlipType.Horizontal);  //水平反转图像以便更符合日常习惯
             });
         }
 
+        /***** 显示项目复选框 点击事件 ****/
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            showBoxs = checkBox1.Checked;
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            showPoints = checkBox2.Checked;
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            showKeyPoint = checkBox3.Checked;
+        }
+
+
+        //////////////////
+        /// 摄像机操作 ///
+        //////////////////
+        
+        /***** 打开摄像头 ****/
         private void OpenCamera()
         {
             try
@@ -90,144 +104,137 @@ namespace HelloOpenCV
             }
         }
 
+
+        ////////////////////
+        /// 图像处理操作 ///
+        ////////////////////
+
+        /***** 画关键点 ****/
         private Image<Bgr, byte> DrawKeyPoint(Image<Bgr, byte> img)
         {
-            if (current_point.TTL >= 50)
+            if (current_point.TTL >= 50)   //如果当前关键点的生存时间大于50 (表示它连续好多帧被识别为关键点)
+            {                              //则绘制当前关键点 (蓝色)
+                current_point.TTL -= 10;   //并将当前关键点的生存时间-10 (及时淘汰老旧的关键点, 提高精确度)
+                img.Draw(new CircleF(new PointF((float)current_point.X, (float)current_point.Y), 5), new Bgr(255, 255, 0), 3);  //绘制当前关键点
+                label1.Text = String.Format("{0, 3}, {1, 3}, {2, 3}", current_point.TTL, (int)current_point.X, (int)current_point.Y);
+            } else
             {
-                current_point.TTL -= 10;
-                img.Draw(new CircleF(new PointF((float)current_point.X, (float)current_point.Y), 5), new Bgr(255, 255, 0), 3);
+                label1.Text = "No Key Point";
             }
             return img;
         }
 
+        /***** 画检测点 ****/
         private Image<Bgr, byte> DrawPoints(Image<Bgr, byte> img)
         {
-            label1.Text = "";
-            foreach (MyPoint point in points)
+            listBox1.Items.Clear(); //清空列表框
+            listBox1.Items.Add("生存时间, 横坐标, 纵坐标"); //清空列表框
+            foreach (MyPoint point in points)  //遍历points数组
             {
-                label1.Text += point.TTL + ", " + point.X + ", " + point.Y + "\n";
-                if (point.TTL >= 50)
-                {
-                    img.Draw(new CircleF(new PointF((float)point.X, (float)point.Y), 5), new Bgr(0, double.MaxValue, 0), 3);
+                listBox1.Items.Add(String.Format("{0, 8}, {1, 6}, {2, 6}", point.TTL, (int)point.X, (int)point.Y));  //添加每个point到列表框
+                if (point.TTL >= 50)  //如果此point的生存时间大于50
+                {                     //则把它画出来
+                    img.Draw(new CircleF(new PointF((float)point.X, (float)point.Y), 5), new Bgr(0, double.MaxValue, 0), 3);  //绘制此point
                 }
             }
             return img;
         }
-
-        private Image<Bgr, byte> ProcessImage(Image<Bgr, byte> img)
-        {
-            var grayframe = img.Convert<Gray, byte>();
-            var faces = cascadeClassifier.DetectMultiScale(grayframe);
-
-            foreach (var face in faces)
-            {
-                float X = (face.Left + face.Right) / 2;
-                float Y = (face.Top + face.Bottom) / 2;
-                float W = (face.Right - face.Left) / 2;
-                //float H = (face.Bottom - face.Top) / 2;
-                //img.Draw(face, new Bgr(0, double.MaxValue, 0), 3);
-                img.Draw(new CircleF(new PointF(X, Y), W / 3), new Bgr(0, double.MaxValue, 0), (int)W);
-            }
-            return img;
-        }
-
-        List<MyPoint> points = new List<MyPoint>();
-        MyPoint current_point = new MyPoint(0, 0);
-
+        
+        /***** 检测手部 并画框 ****/
         private Image<Bgr, byte> ProcessImageUseDnn(Image<Bgr, byte> img)
         {
-            Mat frame = img.Mat;
+            Mat frame = img.Mat; //获取输入图像的矩阵
 
-            String[] classNames = new String[]
-            {
-                "",
-                "hand"
-            };
+            Mat inputBlob = DnnInvoke.BlobFromImage(frame, 1, new Size(300, 300)); //转换为神经网络的输入格式 (300x300)
 
-            Mat inputBlob = DnnInvoke.BlobFromImage(frame, 1, new Size(300, 300));
+            net.SetInput(inputBlob, "image_tensor"); //输入数据
+            Mat prob = net.Forward("detection_out"); //获取输出
 
-            net.SetInput(inputBlob, "image_tensor");
-            Mat prob = net.Forward("detection_out");
-
-            byte[] data = new byte[5600];
+            byte[] data = new byte[5600];  //将输出从矩阵转换为数组, 便于处理
             prob.CopyTo(data);
 
-            //draw result
-            for (int i = 0; i < prob.SizeOfDimemsion[2]; i++)
+            for (int i = 0; i < prob.SizeOfDimemsion[2]; i++) //遍历检测框
             {
-                var d = BitConverter.ToSingle(data, i * 28 + 8);
-                if (d > 0.3)
+                var d = BitConverter.ToSingle(data, i * 28 + 8);  //获取检测框置信度
+                if (d > 0.3) //如果置信度大于30%, 则认为有效
                 {
-                    var idx = (int)BitConverter.ToSingle(data, i * 28 + 4);
-                    var w1 = (int)(BitConverter.ToSingle(data, i * 28 + 12) * img.Width);
-                    var h1 = (int)(BitConverter.ToSingle(data, i * 28 + 16) * img.Height);
-                    var w2 = (int)(BitConverter.ToSingle(data, i * 28 + 20) * img.Width);
-                    var h2 = (int)(BitConverter.ToSingle(data, i * 28 + 24) * img.Height);
+                    var idx = (int)BitConverter.ToSingle(data, i * 28 + 4); //输出的类别, 这里只有hand一个类别
+                    var w1 = (int)(BitConverter.ToSingle(data, i * 28 + 12) * img.Width);   //检测框的位置 - 左侧
+                    var h1 = (int)(BitConverter.ToSingle(data, i * 28 + 16) * img.Height);  //检测框的位置 - 顶部
+                    var w2 = (int)(BitConverter.ToSingle(data, i * 28 + 20) * img.Width);   //检测框的位置 - 右侧
+                    var h2 = (int)(BitConverter.ToSingle(data, i * 28 + 24) * img.Height);  //检测框的位置 - 底部
 
-                    double X = (w1 + w2) / 2;
-                    double Y = (h1 + h2) / 2;
+                    double X = (w1 + w2) / 2; //检测框的位置 - 中心横坐标
+                    double Y = (h1 + h2) / 2; //检测框的位置 - 中心纵坐标
 
-                    AddPoint(X, Y); //添加点
+                    AddPoint(X, Y); //调用添加检测点方法
 
-                    if (false)
+                    if (!showBoxs)  //如果没有开启检测框绘制功能, 则跳过绘制检测框部分
                     {
                         continue;
                     }
 
-                    var label = $"{classNames[idx]} {d * 100:0.00}%";
+                    var label = $"hand {d * 100:0.00}%";   //标签文本: "类别(hand) + 置信度"
 
-                    CvInvoke.Rectangle(img, new Rectangle(w1, h1, w2 - w1, h2 - h1), new MCvScalar(0, 255, 0), 2);
+                    CvInvoke.Rectangle(img, new Rectangle(w1, h1, w2 - w1, h2 - h1), new MCvScalar(0, 255, 0), 2); //新建一个矩形对象来表示检测框
 
                     int baseline = 0;
-                    var textSize = CvInvoke.GetTextSize(label, FontFace.HersheyTriplex, 0.5, 1, ref baseline);
-                    var y = h1 - textSize.Height < 0 ? h1 + textSize.Height : h1;
-                    CvInvoke.Rectangle(img, new Rectangle(w1, y - textSize.Height, textSize.Width, textSize.Height), new MCvScalar(0, 255, 0), -1);
-                    CvInvoke.PutText(img, label, new Point(w1, y), FontFace.HersheyTriplex, 0.5, new Bgr(0, 0, 0).MCvScalar);
+                    var textSize = CvInvoke.GetTextSize(label, FontFace.HersheyTriplex, 0.5, 1, ref baseline);     //计算标签大小
+                    var y = h1 - textSize.Height < 0 ? h1 + textSize.Height : h1; //计算标签位置
+                    CvInvoke.Rectangle(img, new Rectangle(w1, y - textSize.Height, textSize.Width, textSize.Height), new MCvScalar(0, 255, 0), -1);  //绘制检测框
+                    CvInvoke.PutText(img, label, new Point(w1, y), FontFace.HersheyTriplex, 0.5, new Bgr(0, 0, 0).MCvScalar); //绘制标签
                 }
             }
 
-            UpdateTTL();
+            UpdateTTL(); //调用更新生存时间方法
 
             return img;
         }
-        /// <summary>
-        /// ///////////////////////////////////////////////////
-        /// </summary>
+
+
+        //////////////////////
+        /// 检测点处理操作 ///
+        //////////////////////
+
+        private List<MyPoint> points = new List<MyPoint>();
+        private MyPoint current_point = new MyPoint(0, 0);
+
+        /***** 更新(减小)每个检测点的生存时间, 并找出此帧的关键点 ****/
         public void UpdateTTL()
         {
             MyPoint max_point = null;
-            for (int i = points.Count - 1; i >= 0; i--) //倒序遍历删除旧Point
+            for (int i = points.Count - 1; i >= 0; i--) //倒序遍历points数组
             {
                 MyPoint point = points[i];
-                point.TTL -= 10;
-                if (!point.IsAlive)
+                point.TTL -= 10; //将每个检测点的生存时间减10
+                if (!point.IsAlive) // 生存时间 < 0 时
                 {
-                    points.Remove(point);
+                    points.Remove(point); //删除检测点
                     continue;
                 }
-                if (current_point.TTL < 50)
-                {
+                if (current_point.TTL < 50) //当前没有关键点时
+                {                           //直接把此点当作关键点
                     current_point.TTL = 90;
                     current_point.X = point.X;
                     current_point.Y = point.Y;
                     continue;
                 }
-                if (!IsOnePoint(current_point.X, current_point.Y, point.X, point.Y, 50))
-                {
-                    continue;
+                if (!IsOnePoint(current_point.X, current_point.Y, point.X, point.Y, 50)) //当前有关键点时,
+                {                                                                        //判断此点距离关键点是否太远,
+                    continue;                                                            //太远则肯定不会是下一个关键点
                 }
-                if (max_point == null)
-                {
+                if (max_point == null) //当前没有最佳候选关键点时
+                {                      //直接把此点当作最佳候选关键点
                     max_point = point;
                 }
                 else
                 {
-                    if (point.TTL > max_point.TTL)
+                    if (point.TTL > max_point.TTL) //取 此点 与 最佳候选关键点 中生存时间最大的为下一个最佳候选关键点
                     {
                         max_point = point;
                     }
-                    else if (point.TTL == max_point.TTL)
-                    {
+                    else if (point.TTL == max_point.TTL) //此点 与 最佳候选关键点 生存时间相同时
+                    {                                    //取 (此点) 与 (最佳候选关键点) 中距离 (当前关键点) 最近的的为下一个最佳候选关键点
                         double s_point = Math.Abs(current_point.X - point.X) + Math.Abs(current_point.Y - point.Y);
                         double s_max_point = Math.Abs(current_point.X - max_point.X) + Math.Abs(current_point.Y - max_point.Y);
                         if (s_max_point < s_point)
@@ -237,43 +244,51 @@ namespace HelloOpenCV
                     }
                 }
             }
-            if (max_point != null)
-            {
-                current_point.TTL += 20;
+            if (max_point != null)            //如果找到了本帧的 最佳候选关键点
+            {                                 //则把它作为 当前关键点
+                current_point.TTL += 20;      //并把它的生存时间加20
                 current_point.X = max_point.X;
                 current_point.Y = max_point.Y;
             }
         }
 
+        /***** 添加检测点 ****/
         public void AddPoint(double x, double y)
         {
             bool isAdded = false;
-            foreach (MyPoint point in points)
+            foreach (MyPoint point in points) //与当前points数组里的点逐个比较
             {
-                if (IsOnePoint(x, y, point.X, point.Y))
-                {
-                    point.TTL += 20;
+                if (IsOnePoint(x, y, point.X, point.Y)) //如果距离足够近
+                {                                       //则当作同一个点, 不再添加新的点
+                    point.TTL += 20;                    //并给此point的生存时间加20
                     point.X = (x + point.X) / 2;
                     point.Y = (y + point.Y) / 2;
                     isAdded = true;
                     break;
                 }
             }
-            if (!isAdded)
-            {
-                MyPoint point = new MyPoint(x, y);
+            if (!isAdded)                           //如果points数组中没有任何点距离新的点足够近
+            {                                       //则把它当新的点, 添加进points数组
+                MyPoint point = new MyPoint(x, y);  //并将此新的点的生存时间设为30 (并不会立即显示, 大于50才会显示)
                 point.TTL = 30;
                 points.Add(point);
             }
         }
 
+        /***** 判断两点距离是否过近, 可以被看作为一个点 ****/
         public bool IsOnePoint(double x1, double y1, double x2, double y2, double size = 50)
         {
             return x1 < x2 + size && x1 > x2 - size &&
                 y1 < y2 + size && y1 > y2 - size;
         }
+
     }
 
+
+    ///////////////////////
+    /// 自定义MyPoint类 ///
+    ///////////////////////
+    
     public class MyPoint
     {
         public double X;
